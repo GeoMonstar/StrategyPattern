70ae15b3f566f051ef6218d19c534f858a4aa87c
Monstar <Monstar@monstarmacbook-pro.local>
2016-08-04 11:09:27 +0800

    Initial Commit

---
+++ StrategyPattern/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StrategyPattern
+//
+//  Created by Monstar on 16/8/4.
+//  Copyright © 2016年 Monstar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
